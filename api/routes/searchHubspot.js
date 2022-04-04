472e657b4ea04a9c7d1a473f188c9ed409203c61
Jan Rundt <jan.rundt@gmail.com>
2022-04-04 10:56:30 +0300

    merge docs

--- api/routes/searchHubspot.js
+++ api/routes/searchHubspot.js
@@ -1,17 +1,25 @@
 const hubspotController = require('../controllers/hubspotController')
 
-module.exports = async function (event) {
+/**
+ * A function that takes care of requests of type 'POST route=searchDeals' containing the
+ * parameter to use as a query-string.
+ * @param {*} event an object passed as parameters to the lambda, contains
+ * info on the query to use as a search parameter. 
+ * @returns the response from Hubspot or an error.
+ */
+ module.exports = async function (event) {
+  console.log(event)
   console.log(event.body)
   let data = event.body
-  let buff = new Buffer(data, 'base64')
-  const reqObj = JSON.parse(buff.toString('ascii'))
+  let buff = Buffer.from(data.toString('ascii'), 'base64')
+  const reqObj = JSON.parse(buff)
   console.log(reqObj)
 
   try {
-    const result = await hubspotController.searchDeals(reqObj.queryString)
-    return result
+      const result = await hubspotController.searchDeals(reqObj.queryString)
+      return result
   } catch (error) {
-    console.log(error)
-    return {error: error.message}
+      console.log(error)
+      return {error: error.message}
   }
 }
